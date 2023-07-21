247a8abaf77f3ec9f41ad635edc8e6af0a90d450
YavelRico <133065935+YavelRico@users.noreply.github.com>
2023-07-20 21:14:58 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -24,3 +24,66 @@ function convertir() {
 function convertirMoneda(monto, tasa) {
     return monto * tasa;
 }
+
+
+//Info de productos
+const productos = [
+    {
+        id: "producto1",
+        nombre: "Dibujo Tradicional",
+        precioUSD: 15,
+        precioARS: 7500,
+    },
+    {
+        id: "producto2",
+        nombre: "Dibujo Digital",
+        precioUSD: 20,
+        precioARS: 10000,
+    },
+    {
+        id: "producto3",
+        nombre: "Diseño de logotipos",
+        precioUSD: 80,
+        precioARS: 40000,
+    },
+    {
+        id: "servicio1",
+        nombre: "Modelaje 3D",
+        precioUSD: 120,
+        precioARS: 60000,
+    },
+    {
+        id: "servicio2",
+        nombre: "Contratar",
+        precioUSD: 20,
+        precioARS: 10000,
+    },
+];
+
+// Mostrar alert con la información del producto ingresado por el usuario
+function mostrarInformacionProducto() {
+    const nombreProducto = prompt("Ingrese el nombre del producto deseado:");
+
+    if (nombreProducto === null) {
+        alert("Ha cancelado la solicitud.");
+        return;
+    }
+
+    const selectedProduct = productos.find(
+        producto => producto.nombre.toLowerCase() === nombreProducto.toLowerCase()
+    );
+
+    if (selectedProduct) {
+        const info = `ID: ${selectedProduct.id}\nNombre: ${selectedProduct.nombre}\nPrecio USD: $${selectedProduct.precioUSD}\nPrecio ARS: $${selectedProduct.precioARS}`;
+        alert(info);
+    } else {
+        alert("Producto no encontrado o nombre inválido. Intente nuevamente.");
+    }
+}
+
+// Llamar a la función para mostrar la información cuando se carga la página
+window.onload = function () {
+    setTimeout(function () {
+        mostrarInformacionProducto();
+    }, 500);
+};
